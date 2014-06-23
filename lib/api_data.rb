5a24792e7dc6675c8ca5ecca0270f6f170c3cdf3
Sergey Alekseev <sergey.alekseev.minsk@gmail.com>
2014-06-23 21:16:09 +0300

    add facet filters

--- lib/api_data.rb
+++ lib/api_data.rb
@@ -16,12 +16,24 @@ module APIData
     url = "#{API_URL}/funders/#{id}/works?facet=t&rows=#{query_rows}"
     url << "&offset=#{offset}" if offset != 0
     url << "&sort=#{params[:sort]}" if params[:sort]
+    url << filters
     get_message(url)
   end
 
+  private
+    def filters
+      facets = Facets::FACETS_MAPPING.keys.select { |key| params.has_key? key }
+      if facets.empty?
+        ''
+      else
+        filters = []
+        facets.delete 'type' # from client: need to resolve some issues with type names and type IDs. Stay tuned.
+        filters << "from-pub-date:#{params[:year]},until-pub-date:#{params[:year]}" if facets.delete 'year'
+        filters << facets.map { |facet| "#{Facets::FACETS_MAPPING[facet]}:#{CGI.escape(params[facet])}" }
+        "&filter=#{filters.join(',')}"
+      end
     end
 
-  private
     def get_message(url)
       uri = URI.parse(url)
       response = Net::HTTP.get_response(uri)

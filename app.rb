4b95cd3ce6359784e55bb5003bd260cd58cf02dc
Sergey Alekseev <sergey.alekseev.minsk@gmail.com>
2014-06-23 19:45:51 +0300

    reduce #page parameters count and extract logic

--- app.rb
+++ app.rb
@@ -6,16 +6,19 @@ Dir["lib/*.rb"].each { |file| require_relative file }
 helpers do
   include RenderingConcerns, APIData, QueryParams
 
-  def page(funder, results, total_results)
     funder_info = { nesting: funder['hierarchy'],
                     nesting_names: funder['hierarchy-names'],
                     id: funder['id'] }
     { funder: funder_info,
+  def results(funder, works)
+    SearchResult.results_from_items(works['items'], funder['hierarchy-names'])
+  end
+  def page(funder, works)
       bare_query: funder['name'],
       query: funder['name'],
       page: query_page,
-      items: results,
-      paginate: Paginate.new(query_page, query_rows, total_results),
+      items: results(funder, works),
+      paginate: Paginate.new(query_page, query_rows, works['total-results']),
       facets: ['category', 'type', 'year', 'publication', 'publisher', 'source'] }
   end
 end
@@ -24,13 +27,12 @@ get '/' do
   if params.has_key?('q')
     funder = funder_hash(params['q'])
     works = funder_works_hash(params['q'], params)
-    results = SearchResult.results_from_items(works['items'], funder['hierarchy-names'])
 
     if params['format'] == 'csv'
       content_type 'text/csv'
-      SearchResult.generate_csv(results)
+      SearchResult.generate_csv results(funder, works)
     else
-      haml :results, locals: { page: page(funder, results, works['total-results']) }
+      haml :results, locals: { page: page(funder, works) }
     end
   else
     haml :splash, locals: { page: {} }
